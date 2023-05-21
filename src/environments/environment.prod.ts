caca3c82bc761ec44c51e74929211932b18383a3
user <votre_adresse_e-mail_GitHub>
2023-05-21 20:54:44 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+
+    firebaseConfig : {
+    apiKey: "AIzaSyDABwEYCGpRj4XvodDrkDoxBvYW8q6dFhU",
+    authDomain: "management-82345.firebaseapp.com",
+    projectId: "management-82345",
+    storageBucket: "management-82345.appspot.com",
+    messagingSenderId: "1054134719467",
+    appId: "1:1054134719467:web:033520fa1fe6cdd49f7fae"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: false, 
+
   firebaseConfig : {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDABwEYCGpRj4XvodDrkDoxBvYW8q6dFhU",
+    authDomain: "management-82345.firebaseapp.com",
+    projectId: "management-82345",
+    storageBucket: "management-82345.appspot.com",
+    messagingSenderId: "1054134719467",
+    appId: "1:1054134719467:web:033520fa1fe6cdd49f7fae"
   }
+
 };
 
 /*
